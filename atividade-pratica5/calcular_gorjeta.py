c43c723dad2f712a665523401e5b40ee2ebae33a
eusalesr <rafaelasalespereira18@hotmail.com>
2025-10-02 23:15:24 -0300

    Exercicios pratica 05

---
+++ atividade-pratica5/calcular_gorjeta.py
@@ -0,0 +1,12 @@
+# Função que recebe valor da conta e porcentagem desejada de gorjeta(float)e retorna a gorjeta a ser paga(float)
+def calcular_gorjeta(valor_conta: float, porcentagem gorjeta: float) -> float:
+    gorjeta = valor_conta = (porcentagem_gorjeta / 100)
+    return gorjeta
+
+---
+resultado = calcular_gorjeta(100.0, 50.0)
+print(f"Gorjeta: R${resultado:.2f}")
+"""
+
+print(calcular_gorjeta(100.0, 27.0))
+
